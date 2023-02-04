f390bf303fd7c05e01eb6c15ec6b39ebb4b05e45
Mati <matimotof@gmail.com>
2023-02-04 16:22:21 +0000

    Updates

--- ex11_starter.py
+++ ex11_starter.py
@@ -3,17 +3,19 @@
 Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
 # asked to print the length of the string Belgium
 print(len(Belgium))
+
 #created a new variable called 'length' to capture the string length
 length = len(Belgium)
 #printed a hyphen times the length
 print("-" * length)
+
 # use the replace method with the old seperator then comma and the new seperator in speech marks
 print(Belgium.replace(',', ':'))
+
 #created a new variable with the output from the replace method called Belgium1
 Belgium1 = "Belgium:10445852:Brussels:737966:Europe:1830:Euro:Catholicism:Dutch:French:German"
-# created a new variable called parts using the split method on the beligum string - used colon as the deault seperator
+# created a new variable called parts using the split method on the Belgium string - used colon as the default seperator
 parts = Belgium1.split(':')
-# created a new variable coneverting parts 1 and 3 into integers and added them together
+# created a new variable, converted parts 1 and 3 into integers and added them together
 combined_pop = int(parts[1]) + int(parts[3])
 print(combined_pop)
-
